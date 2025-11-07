1a6c162d78e3c84b557f96f9d6aab2cc504cdb32
Ly <tytly0110@gmail.com>
2025-11-07 14:15:33 +0700

    fix: update home page and package config

--- lib/src/modules/home/presentation/home_page.dart
+++ lib/src/modules/home/presentation/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 import '../../../../../generated/colors.gen.dart';
 import 'widgets/post_list.dart';
